d3d2a60f5aa92f23051f3c4e2a12c5c458c57f36
steven278 <darkavenger347@gmail.com>
2022-08-01 16:27:12 +0700

    tutorial 5th video

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const liked = 50;
-  // const person = { name: 'steven', age: 21 };
-  // const bool = true;
-  const link = "http://google.com";
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {liked} times</p>
-        <p>{10}</p>
-        <p>{"Hello, ninjas"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
     </div>
   );
