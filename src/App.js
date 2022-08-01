4d24c0d8ae49af2cffbdaee169409d43a498babf
steven278 <darkavenger347@gmail.com>
2022-08-01 15:50:19 +0700

    react tutorial 4th video

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const liked = 50;
+  // const person = { name: 'steven', age: 21 };
+  // const bool = true;
+  const link = "http://google.com";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {liked} times</p>
+        <p>{10}</p>
+        <p>{"Hello, ninjas"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+
+        <a href={link}>Google Site</a>
+      </div>
     </div>
   );
 }
