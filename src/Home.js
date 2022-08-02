b9245ecc9f28683660c1e3e5b5a70d0a7ae6bec6
steven278 <darkavenger347@gmail.com>
2022-08-02 15:38:13 +0700

    tutorial 20th video

--- src/Home.js
+++ src/Home.js
@@ -1,37 +1,16 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+    const { data, isPending, error } = useFetch('http://localhost:8000/blogs');
 
-    useEffect(() => {
-        setTimeout(() => { //just for simulation
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if (!res.ok) {
-                        throw new Error('could not fetch the data for that resource');
-                    }
-                    return res.json();
-                })
-                .then((data) => {
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch(err => {
-                    setIsPending(false);
-                    setError(err.message);
-                })
-        }, 1000);
-    }, []);
 
     return (
         <div className="home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+            {data && <BlogList blogs={data} title="All Blogs!" />}
         </div>
     );
 }
---
+++ src/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => { //just for simulation
+            fetch(url)
+                .then(res => {
+                    if (!res.ok) {
+                        throw new Error('could not fetch the data for that resource');
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    setData(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
+        }, 1000);
+    }, []);
+    return { data, isPending, error };
+}
+
+export default useFetch;
