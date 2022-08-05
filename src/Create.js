1cbc660865e711ccded4d163859cee53c9e1c129
steven278 <darkavenger347@gmail.com>
2022-08-05 12:02:03 +0700

    tutorial 30th video

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -17,6 +19,8 @@ const Create = () => {
         }).then(() => {
             console.log('new blog added');
             setIsPending(false);
+            // history.go(-1)//back 1 through history
+            history.push('/');
         })
     }
 
