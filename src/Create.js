a9180ca3fa80d1c4699d6095a4752ca81ca8609f
steven278 <darkavenger347@gmail.com>
2022-08-05 11:20:29 +0700

    tutorial 28th video

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,17 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+        console.log(blog)
+    }
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
                 <label>Blog body:</label>
@@ -17,7 +24,7 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                <button type="submit">Add Blog</button>
                 {/* <p>{title}</p> */}
                 {/* <p>{body}</p> */}
                 {/* <p>{author}</p> */}
